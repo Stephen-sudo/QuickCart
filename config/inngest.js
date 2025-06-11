d910c8c6020b14262eba7ce9bf0bc7424ff4130f
Stephen-sudoa <nanagyasi180@gmail.com>
2025-06-11 23:12:39 +0100

    fixed a bug

--- config/inngest.js
+++ config/inngest.js
@@ -14,13 +14,12 @@ export const syncUserCreation = inngest.createFunction(
     },
     {event:"clerk/user.created"},
     async({event})=>{
-        const {id, first_name, last_name, email_addresses,image_url} = event.data;
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
         const userData = {
             _id:id,
             email: email_addresses[0].email_address,
             name: first_name + " " + last_name,
-            image: image_url ,
-
+            image: image_url,
         }
         await connectDB();
         await User.create(userData);
@@ -41,7 +40,7 @@ export const syncUserUpdation = inngest.createFunction(
             image: image_url,
         };
         await connectDB();
-        await User.findByIdAndUpdate(id, userData, { new: true });
+        await User.findByIdAndUpdate(id, userData);
     }
 )
 
@@ -51,6 +50,7 @@ export const syncUserDeletion = inngest.createFunction(
     {event:"clerk/user.deleted"},
     async ({event}) => {
         const {id} = event.data;
+        
         await connectDB();
         await User.findByIdAndDelete(id);
     }
