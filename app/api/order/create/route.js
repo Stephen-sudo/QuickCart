9e8a20e55c160f468bebca872afc6484ea0e6d12
Stephen-sudoa <nanagyasi180@gmail.com>
2025-06-26 13:06:40 +0100

    refactor: update order creation logic and schema for consistency

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,9 +1,10 @@
+import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+export async function POST(request) {
   try {
     const { userId } = getAuth(request);
     const { address, items } = await request.json();
@@ -14,10 +15,12 @@ export async function GET(request) {
       });
     }
     // calculate amount using items
-    const amount = items.reduce(async (acc, item) => {
-      const product = await Product.findById(item.productId);
-      return acc + product.price * item.quantity;
-    }, 0);
+    let amount = 0;
+    for (const item of items) {
+      const product = await Product.findById(item.product);
+      amount += product.offerPrice * item.quantity;
+    }
+
     await inngest.send({
       name: "order/created",
       data: {
--- models/Order.js
+++ models/Order.js
@@ -4,13 +4,13 @@ const orderSchema = new mongoose.Schema({
   userId: { type: String, required: true, ref: "user" },
   items: [
     {
-      productId: { type: String, required: true, ref: "Product" },
+      product: { type: String, required: true, ref: "product" },
       quantity: { type: Number, required: true },
     },
   ],
   amount: { type: Number, required: true },
   address: { type: String, ref: "address", required: true },
-  status: { type: String, default: "pending" },
+  status: { type: String, required: true, default: "Order Placed" },
   date: { type: Number, required: true },
 });
 
